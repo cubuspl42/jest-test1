22dba6f8c7f4bacf80573db9eded89b426ef3b87
Jakub Trzebiatowski <cubuspl42@gmail.com>
2019-11-18 22:39:26 +0100

    Add TextEncoder test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import * as app from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('manually encoded string equals to the expected byte array', () => {
+  expect(app.getEncodedFoo()).toEqual(new Uint8Array([102, 111, 111]));
+});
+
+it('string encoded with TextEncoder equals to the expected byte array', () => {
+  expect(app.encodeFoo()).toEqual(new Uint8Array([102, 111, 111]));
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function getEncodedFoo() {
+  return new Uint8Array([102, 111, 111]);
 }
 
-export default App;
+export function encodeFoo() {
+  return new TextEncoder().encode("foo");
+}
